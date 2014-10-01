271fffe970459939bfae09f372dc414c4526dedd
Luke Zbikowski <luke.zbikowski@intecco.com.au>
2014-10-01 20:46:47 +1000

    Fixed bootstarap.sh - changed project directory to /vagrant - removed project bundling

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,5 @@ rbenv global 2.1.2
 rbenv local 2.1.2
 rbenv rehash
 
-git clone https://github.com/lukezbikowski/skillscourt-api /vagrant
+git clone https://github.com/lukezbikowski/skillscourt-api /vagrant/skillscourt-api
 gem install bundler
-cd /vagrant
-bundle install --without=production
